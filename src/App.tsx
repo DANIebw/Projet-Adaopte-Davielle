e54ea6e4dba05edcb7771b1447a78637ed1bd547
DANIELLE EBWEA SONGUE <danielle.ewea237@gmail.com>
2026-01-09 11:57:35 +0100

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./Components/Layout/Navbar.tsx";
+import HomePage from "./pages/HomePage";
+import AdaoptPage from "./pages/AdaoptPage";
+
 export default function App() {
-  return <div className="text-9xl">Bienvenue sur Adopte Davielle !</div>;
+  return (
+    <>
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/animals" element={<AdaoptPage />} />
+      </Routes>
+    </>
+  );
 }
