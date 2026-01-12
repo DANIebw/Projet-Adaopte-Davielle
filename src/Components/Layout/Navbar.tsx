816cea46c6d57499b22e8c7c0091cdf17416f33a
DANIELLE EBWEA SONGUE <danielle.ewea237@gmail.com>
2026-01-12 16:49:10 +0100

    fix: change Animals page route

--- src/Components/Layout/Navbar.tsx
+++ src/Components/Layout/Navbar.tsx
@@ -1,9 +1,8 @@
 // import React from "react";
 import { NavLink } from "react-router-dom";
 // NavLink sert à naviguer entre les pages sans recharger le site. C’est l’équivalent React de <a href="">, mais en mieux.
-import { FiHome, FiBookOpen } from "react-icons/fi";
-import { FcSearch } from "react-icons/fc";
-import { FaPaw, FaHeart } from "react-icons/fa";
+import { FiHome } from "react-icons/fi";
+import { FaBone } from "react-icons/fa";
 import { LuRabbit, LuFish } from "react-icons/lu";
 
 export default function Navbar() {
@@ -17,26 +16,79 @@ export default function Navbar() {
         borderBottom: "1px solid rgba(0,0,0,0.06)",
       }}
     >
-      <nav style={{ padding: "12px 16px", borderBottom: "1px solid #ddd" }}>
-        <div style={{ display: "flex", gap: 16, alignItems: "center" }}>
-          <span style={{ fontSize: 25 }}>
-            <strong>🐾Ada</strong>
-            <strong style={{ color: "#ff6d38" }}>opte</strong>
-          </span>
-
-          <NavLink to="/" style={{ textDecoration: "none" }}>
-            <FiHome size={18} />
-            Accueil
+      <nav
+        style={{
+          padding: "12px 16px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div style={{ fontSize: 25 }}>
+          <strong>🐾Ada</strong>
+          <strong style={{ color: "#ff6d38" }}>opte</strong>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: 24,
+            alignItems: "center",
+          }}
+        >
+          <NavLink to="/">
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <FiHome size={18} />
+              <strong>Accueil</strong>
+            </div>
           </NavLink>
-          <NavLink to="/animals" style={{ textDecoration: "none" }}>
-            j'adopte
+          <NavLink
+            to="/animals"
+            aria-label="Page J'adopte"
+            // style={{ textDecoration: "none" }}
+          >
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <FaBone />
+              <strong>j'adopte</strong>
+            </div>
           </NavLink>
-          <NavLink to="/" style={{ textDecoration: "none" }}>
-            Guide l'adoption
+          <NavLink to="/Guide">
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <LuFish size={18} />
+              <strong>Guide l'adoption</strong>
+            </div>
           </NavLink>
-          <NavLink to="/" style={{ textDecoration: "none" }}>
-            Devenir bénévole
+          <NavLink to="/benevole">
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <LuRabbit size={18} />
+              <strong>Devenir bénévole</strong>
+            </div>
           </NavLink>
+        </div>
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          <button
+            type="button"
+            aria-label="Faire un don"
+            style={{
+              padding: "10px 16px",
+              borderRadius: 999,
+              border: "none",
+              background: "#f15b4a",
+              color: "white",
+              fontWeight: 700,
+              cursor: "pointer",
+              display: "flex",
+              alignItems: "center",
+              gap: 20,
+            }}
+            onClick={() => console.log("Don cliqué")}
+          >
+            Faire un don🫶
+          </button>
         </div>
       </nav>
     </header>
