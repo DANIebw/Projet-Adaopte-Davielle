fc5ccced0bc327d2394eee25ded67d31f542278b
DANIELLE EBWEA SONGUE <danielle.ewea237@gmail.com>
2026-01-15 09:59:36 +0100

    create pagination

--- src/adopt/FiltersBar.tsx
+++ src/adopt/FiltersBar.tsx
@@ -10,7 +10,7 @@ export default function FiltersBar() {
           <strong>type d'animal</strong>
         </div>
       </div>
-      {/* <div className="relative mb-4 flex w-full flex-wrap items-stretch"> */}
+      <div className="relative mb-4 flex w-full flex-wrap items-stretch">
         <input
           type="search"
           className="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
